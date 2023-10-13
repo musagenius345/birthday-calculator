69c8bbfd3b33fca629faeaf557a98029d5826968
musagenius345 <musabadru@gmail.com>
2023-10-13 22:00:07 +0300

    removed eruda

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,10 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&display=swap" rel="stylesheet">
-<script src="node_modules/eruda/eruda.js"></script>
-<script>
-$:eruda.init();
-</script>
+<!-- <script src="node_modules/eruda/eruda.js"></script> -->
+<!-- <script> -->
+<!-- // $:eruda.init(); -->
+<!-- // </script> -->
 
 </svelte:head>
 <main class="container">
