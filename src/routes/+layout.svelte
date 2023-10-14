56f8f93980404463292f35eef45ece9c041e55d3
musagenius345 <musabadru@gmail.com>
2023-10-14 17:20:48 +0300

    added title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../pico.css'
 </script>
   <svelte:head>
+  <title>Age Calculator</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&display=swap" rel="stylesheet">
