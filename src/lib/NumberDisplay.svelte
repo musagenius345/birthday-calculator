05884df421f5fad1aea2c20e2a4f6e6b27049152
musagenius345 <musabadru@gmail.com>
2023-10-14 18:35:33 +0300

    animating the displayed age
    will probable change the transitions

--- src/lib/NumberDisplay.svelte
+++ src/lib/NumberDisplay.svelte
@@ -1,16 +1,33 @@
-<script lang="ts">
-  export let value: string
-  export let units: strings
-  export let size 
+<script>
+  import { slide, blur } from 'svelte/transition';
+  import { linear } from 'svelte/easing';
+
+  export let value;
+  export let units;
+
+  let previousValue = null;
+
+  const options = {
+        duration: 500, // Animation duration in milliseconds
+        easing: linear // Easing function for the animation
+    }
 </script>
-<div style="display: contents">
+
+<div> 
   <p>
-    <span class="num">{value}</span> {units}</p>
+    {#key value}
+    <span class="num" in:slide={options} >{value}</span> 
+    {/key}
+    <span>{units}</span>
+  </p>
 </div>
+
 <style>
+  .num {
+    display: inline-block;
+  }
   p{
-    --font-size: 2rem;
-    font-size: 3rem;
+    font-size: 3.3rem;
     font-weight: 700;
     color: var(--smoke-grey);
 
